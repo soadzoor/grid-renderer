da02d6e5c9fcc9b300fb849240b028d2adcfee45
Peter Varga <soadzoor@gmail.com>
2021-06-17 17:11:23 +0200

    Added online demo

--- src/ts/utils/Constants.ts
+++
@@ -1,6 +0,0 @@
-export class Constants
-{
-	// https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button
-	public static readonly MAIN_MOUSE_BUTTON: number = 0;
-	public static readonly SECONDARY_MOUSE_BUTTON: number = 2;
-}
--- src/ts/utils/Functions.ts
+++
@@ -1,4 +0,0 @@
-export class Functions
-{
-	public static emptyFunction() {};
-}
